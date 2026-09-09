f5434d6cd01e704d508b8cc383d36a31e40c9f2d
Sororain <m15055510xx@gmail.com>
2026-09-10 00:51:46 +0800

    feat: 正常工作区最大化

--- lib/common/window.dart
+++ lib/common/window.dart
@@ -35,7 +35,7 @@ class Window {
     if (!system.isMacOS || version > 10) {
       await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
     }
-    await windowManager.setMaximizable(false);
+    await windowManager.setMaximizable(true);
     await _windowPosition(props);
     await windowManager.waitUntilReadyToShow(windowOptions, () async {
       await windowManager.setPreventClose(true);
